4ac79d513de5cddee363bfc3a537275e06a87ab3
CocoHoneyWaffle <ohleo199@mindattic.com>
2026-04-11 13:57:33 -0500

    commit

--- TaxRateCollector.Core/obj/Debug/net10.0/TaxRateCollector.Core.AssemblyInfo.cs
+++ TaxRateCollector.Core/obj/Debug/net10.0/TaxRateCollector.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaxRateCollector.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ce6b9fc433cb8270767a79180f480cb667a45b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45a73b31ccb7aa9c48a4548c77b0b96d4921da41")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaxRateCollector.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaxRateCollector.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
